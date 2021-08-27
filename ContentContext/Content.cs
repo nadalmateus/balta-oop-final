39d7c76074ee680e1ed3adf3351ca7686fa58715
Mateus Nadal <nadalmateus@hotmail.com>
2021-08-27 14:58:58 -0300

    Initial Commit

---
+++ ContentContext/Content.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Balta.ContentContext
+{
+    public class Content
+    {
+        public Content()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Balta.ContentContext;
+
+namespace balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var course = new Course();
+
+        }
+    }
+}
