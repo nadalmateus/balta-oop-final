982a14b12c898c697a68c31de2f4bfd6c2097840
Mateus Nadal <nadalmateus@hotmail.com>
2021-12-04 22:23:29 -0300

    feat: constructor method

--- ContentContext/Article.cs
+++ ContentContext/Article.cs
@@ -1,4 +1,10 @@
 namespace csharp.ContextContent
 {
-    public class Article : Content { }
+    public class Article : Content
+    {
+        public Article(string title, string url) : base(title, url)
+        {
+
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿using csharp.ContextContent;
 
+var articles = new List<Article>();
+articles.Add(new Article("C#", "dot.net"));
+articles.Add(new Article("F#", "dot.net"));
+articles.Add(new Article("Java", "java.net"));
 
 
-var course = new Course();
 
+foreach (var article in articles)
+{
+    System.Console.WriteLine(article.Id);
+    System.Console.WriteLine(article.Title);
+    System.Console.WriteLine(article.Url);
 
-Console.WriteLine(course.Id);
+}
