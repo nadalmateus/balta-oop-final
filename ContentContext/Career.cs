478a4fd05e4d3c1ed25e9f82ef4973ed8a042589
Mateus Nadal <nadalmateus@hotmail.com>
2021-12-04 22:14:26 -0300

    feat: modeling the career

--- ContentContext/Career.cs
+++ ContentContext/Career.cs
@@ -2,6 +2,13 @@ namespace csharp.ContextContent
 {
     public class Career : Content
     {
-        public int Courses { get; set; }
+        public Career()
+        {
+            Items = new List<CareerItem>();
+        }
+        public IList<CareerItem> Items { get; set; }
+        public int TotalCourses => Items.Count;
+
     }
+
 }
